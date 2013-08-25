89bdb88d5ad00692dc47e4d0a45ff36811e1d087
Joeler <joel.neland@gmail.com>
2013-08-25 12:52:11 -0500

    First Commit - Secure tokena nd Rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,31 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+# Coliseum::Application.config.secret_key_base = '1d89b6b4e0ed0af729c2c78b2958dfbab2097d5ec6d398466d085bbc8e31b081844f5aa6eefa366218add9a0e55fd6eea6abcbf3ff977df7685a42a7673f049b'
+
+#creating secret token for security purposes as we put this on web
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    #Use existing token
+    File.read(token_file).chomp
+  else
+    #generate and stoer a new token
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Coliseum::Application.config.secret_key_base = secure_token
